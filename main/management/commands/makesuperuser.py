03044c6c1dbf9b7fa25b6b2a2a6a14871bbd289d
ralarcon2019 <76644162+ralarcon2019@users.noreply.github.com>
2024-11-06 19:02:46 -0500

    update

--- main/management/commands/makesuperuser.py
+++ main/management/commands/makesuperuser.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         email = ''
-        username = 'Ralarcon'
+        username = 'Ralarcon2019'
         new_password = get_random_string(10)
         try:
             if not User.objects.filter(is_superuser=True).exists():

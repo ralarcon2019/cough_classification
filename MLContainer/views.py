67e212ddd537b74527227fd1e3c58fead9bcb1ae
ralarcon2019 <76644162+ralarcon2019@users.noreply.github.com>
2025-01-29 12:58:39 -0500

    update.... again

--- MLContainer/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def ML_View(request):
-    return render(request, "MLContainer/main.html")
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-gunicorn cough_classification.wsgi --log-file -
